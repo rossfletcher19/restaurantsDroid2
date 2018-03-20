13feae4b5e0414627cb225b5b32da70ac06c5975
Ross Fletcher <rtbfletch@outlook.com>
2018-03-20 07:37:33 -0700

    add .gradle

--- app/src/main/java/com/epicodus/restaurantsandroid/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.epicodus.restaurantsandroid;
-
-
-public class Constants {
-    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
-    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/businesses/search?term=restaurants";
-    public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
-}
